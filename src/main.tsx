217bf20fc68bc07b369cb30299f2420168fa7bc1
naingkhanthtet <naingkhanthtet15@gmail.com>
2024-12-04 17:07:53 +0700

    applying theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeContextProvider } from "./utils/ThemeContextProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <ThemeContextProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ThemeContextProvider>
+);
