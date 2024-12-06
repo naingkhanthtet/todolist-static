3eb4b152e11784e34e3c799b06eca1cceeea0d84
naingkhanthtet <naingkhanthtet15@gmail.com>
2024-12-06 12:55:56 +0700

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import "./App.css";
-import { Button } from "@mui/material";
-import { useThemeContext } from "./utils/useThemeContext";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Tasks from "./components/tasks/Tasks";
 
 function App() {
-  const { isDarkMode, toggleTheme } = useThemeContext();
-
-  return <Button onClick={toggleTheme}>{isDarkMode ? "Dark" : "Light"}</Button>;
+  return (
+    <>
+      <Nav />
+      <Tasks />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
