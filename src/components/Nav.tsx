831db61ff1b04d3b213ff5a4510753a6dbec132d
naingkhanthtet <naingkhanthtet15@gmail.com>
2024-12-08 11:07:12 +0700

    nav update

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  NavText,
-  GridBox,
-  ThirdIcons,
-  ContentWidth,
-} from "./styles/BasicComponents";
+import { NavText, GridBox, ThirdIcons } from "./styles/BasicComponents";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import { useThemeContext } from "../utils/useThemeContext";
@@ -13,19 +8,20 @@ const Nav: React.FC = () => {
   const { isDarkMode, toggleTheme } = useThemeContext();
 
   return (
-    <ContentWidth
-      sx={{
-        marginTop: 5,
-      }}
-    >
-      <GridBox>
+    <>
+      <GridBox
+        sx={{
+          marginTop: 3,
+          marginBottom: 3,
+        }}
+      >
         <NavText>Todo Tick</NavText>
 
         <ThirdIcons onClick={toggleTheme}>
           {isDarkMode ? <LightModeOutlinedIcon /> : <DarkModeOutlinedIcon />}
         </ThirdIcons>
       </GridBox>
-    </ContentWidth>
+    </>
   );
 };
 
